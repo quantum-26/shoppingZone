0fee41ec9bb86c3e87de306c77df6ceb19d0dc57
quantum-26 <0826pathak@gmail.com>
2022-10-01 22:24:11 +0530

    implement login api
    create login api using nextAuth credential providers and toast message

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
+import { useSession } from 'next-auth/react';
 import Head from 'next/head'
 import Link from 'next/link'
 import React, { useContext, useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Store } from '../utils/Store';
 
 function Layout({ title, children }) {
+  const { status, data: session } = useSession();
   const { state } = useContext(Store);
   const { cart } = state;
   const [cartItemsCount, setCartItemsCount] = useState(0);
@@ -17,6 +21,7 @@ function Layout({ title, children }) {
             <meta name="description" content="Ecommerce website" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
+        <ToastContainer position="bottom-center" limit={1} />
         <div className='flex min-h-screen flex-col justify-between'>
             <header>
                 <nav className='flex h-12 items-center px-4 justify-between shadow-md'>
@@ -35,9 +40,15 @@ function Layout({ title, children }) {
                             )}
                         </a>
                         </Link>
-                        <Link href="/login">
-                            <a className='p-2' >Login</a>
-                        </Link>
+                        {status === 'loading' ? (
+                            'Loading'
+                        ) : session?.user ? (
+                            session.user.name
+                        ) : (
+                            <Link href="/login">
+                            <a className="p-2">Login</a>
+                            </Link>
+                        )}
                     </div>
                 </nav>
             </header>
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,53 @@
+import bcryptjs from 'bcryptjs';
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import User from '../../../models/User';
+import db from '../../../utils/db';
+
+export default NextAuth({
+    // authenticate user using jwt
+    callbacks: {
+        jwt: async ({ token, user }) => {
+            if (user?._id) token._id = user._id;
+            if (user?.isAdmin) token.isAdmin = user.isAdmin;
+            return token;
+        },
+        session: async ({ session, token }) => {
+            if (token?._id) session.user._id = token._id;
+            if (token?.isAdmin) session.user.isAdmin = token.isAdmin;
+            return session;
+        },
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    pages: {
+        signIn: '/login',
+    },
+    // Enable debug messages in the console if you are having problems
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: 'jwt',
+    },
+    providers: [
+        CredentialsProvider({
+            async authorize(credentials) {
+                console.log(credentials.email)
+                console.log(credentials.password)
+                await db.connect();
+                const user = await User.findOne({
+                    email: credentials.email,
+                });
+                await db.disconnect();
+                if (user && bcryptjs.compareSync(credentials.password, user.password)) {
+                    return {
+                        _id: user._id,
+                        name: user.name,
+                        emaipl: user.email,
+                        image: 'f',
+                        isAdmin: user.isAdmin,
+                    };
+                }
+                throw new Error('Invalid email or password');
+            },
+        }),
+    ],
+});
